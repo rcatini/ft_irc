f3d004f9b92ad936df5f45d54f4f98b47c2f4846
rcatini <100608277+rcatini@users.noreply.github.com>
2023-03-28 19:14:06 +0000

    convert to tabs

--- ircserv.cpp
+++ ircserv.cpp
@@ -3,19 +3,19 @@
 
 int main(int argc, char *argv[])
 {
-    if (argc != 3)
-    {
-        std::cout << "Usage: " << argv[0] << "./ircserv <port> <password>" << std::endl;
-        return 1;
-    }
+	if (argc != 3)
+	{
+		std::cout << "Usage: " << argv[0] << "./ircserv <port> <password>" << std::endl;
+		return 1;
+	}
 
-    char *endpointer;
-    int port = std::strtol(argv[1], &endpointer, 10);
-    if (*endpointer != '\0' || port < 0 || port > 65535)
-    {
-        std::cout << "Invalid port number" << std::endl;
-        return 1;
-    }
+	char *endpointer;
+	int port = std::strtol(argv[1], &endpointer, 10);
+	if (*endpointer != '\0' || port < 0 || port > 65535)
+	{
+		std::cout << "Invalid port number" << std::endl;
+		return 1;
+	}
 
-    std::string password = argv[2];
+	std::string password = argv[2];
 }
